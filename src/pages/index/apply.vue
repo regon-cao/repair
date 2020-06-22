feb02f5f08f06704b0c247af7da130ce4a6b4d38
regonCao@163.com <regonCao@163.com>
2020-06-22 23:02:31 +0800

    upup

--- src/pages/index/apply.vue
+++ src/pages/index/apply.vue
@@ -2,7 +2,7 @@
     <view class="">
         <mx-form has-top class="form">
             <mx-field title="报修类别">
-                <mx-picker :range="sourceList" placeholder="请选择报修类别" range-key="text" @change="PickerChange" :value="0" />
+                <mx-picker :range="sourceList" placeholder="请选择报修类别" range-key="text" @change="PickerChange" :value="type" />
             </mx-field>
             <mx-field>
                 <mx-label text="报修位置" />
@@ -21,7 +21,7 @@
                 <mx-input type="text" />
             </mx-field>
         </mx-form>
-        <mx-button type="primary" value="提 交" block ext-class="form-submit" style="margin-top:100rpx" />
+        <mx-button type="primary" value="提 交" block ext-class="form-submit" class="apply" />
     </view>
 </template>
 <script>
@@ -30,21 +30,33 @@ export default {
         return {
             sourceList: [
                 { text: '照明', value: 1 },
-                { text: '空调', value: 1 },
-                { text: '电梯', value: 1 },
-                { text: '清洁', value: 1 },
-                { text: '电脑', value: 1 },
-                { text: '电源', value: 1 },
-                { text: '打印机', value: 1 },
-                { text: '更多', value: 1 }
-            ]
+                { text: '空调', value: 2 },
+                { text: '电梯', value: 3 },
+                { text: '清洁', value: 4 },
+                { text: '电脑', value: 5 },
+                { text: '电源', value: 6 },
+                { text: '打印机', value: 7 },
+                { text: '多媒体设备', value: 8 },
+                { text: '公共清洁', value: 9 },
+                { text: '管道', value: 10 },
+                { text: '门禁', value: 11 },
+                { text: '电梯', value: 12 },
+                { text: '办公家具', value: 13 },
+                { text: '景观设施', value: 14 },
+                { text: '电脑网络', value: 15 }
+            ],
+            type: 0
         };
     },
-    onShow() {
+    onShow(option) {
         this.$Monitor && this.$Monitor.pageShow();
     },
     onHide() {
         this.$Monitor && this.$Monitor.pageHide();
+    },
+    onLoad(option) {
+        this.type = +option.type;
+        console.log(this.type);
     },
     props: {},
     methods: {
@@ -56,5 +68,10 @@ export default {
 <style scoped lang="scss">
 .form {
     margin-bottom: 100rpx;
+}
+.apply {
+    position: absolute;
+    bottom: 10rpx;
+    width: 100%;
 }
 </style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -16,7 +16,7 @@
         />
         <view class="atom-layout">
             <view style="width:25%;margin-bottom:30rpx;">
-                <view class="avatarWrap" @click="avatar_text2_0_0">
+                <view class="avatarWrap" @click="toApply(1)">
                     <mx-avatar
                         src="https://jfile.cloudpnr.com/app-7349e98b-7bef-4b61-b278-48644bbc8058/678749dd715fe7efb808a25b9875e1efrepairman888照明.png"
                         mode="aspectFill"
@@ -32,7 +32,7 @@
                 </view>
             </view>
             <view style="width:25%;margin-bottom:30rpx;">
-                <view class="avatarWrap" @click="avatar_text2_1_0">
+                <view class="avatarWrap" @click="toApply(2)">
                     <mx-avatar
                         src="https://jfile.cloudpnr.com/app-7349e98b-7bef-4b61-b278-48644bbc8058/31299a11a387b669b2a2d09fdd6dd063repairman888空调.png"
                         mode="aspectFill"
@@ -48,7 +48,7 @@
                 </view>
             </view>
             <view style="width:25%;margin-bottom:30rpx;">
-                <view class="avatarWrap" @click="avatar_text2_2_0">
+                <view class="avatarWrap" @click="toApply(3)">
                     <mx-avatar
                         src="https://jfile.cloudpnr.com/app-7349e98b-7bef-4b61-b278-48644bbc8058/380026fca373254fb1fb4d539d2d8bdarepairman888电梯.png"
                         mode="aspectFill"
@@ -64,7 +64,7 @@
                 </view>
             </view>
             <view style="width:25%;margin-bottom:30rpx;">
-                <view class="avatarWrap" @click="avatar_text2_3_0">
+                <view class="avatarWrap" @click="toApply(4)">
                     <mx-avatar
                         src="https://jfile.cloudpnr.com/app-7349e98b-7bef-4b61-b278-48644bbc8058/f81db47c9e0e6719845111139f8b34c6repairman888清洁用具.png"
                         mode="aspectFill"
@@ -80,7 +80,7 @@
                 </view>
             </view>
             <view style="width:25%;margin-bottom:30rpx;">
-                <view class="avatarWrap" @click="avatar_text2_4_0">
+                <view class="avatarWrap" @click="toApply(5)">
                     <mx-avatar
                         src="https://jfile.cloudpnr.com/app-7349e98b-7bef-4b61-b278-48644bbc8058/33abdfe6d978aa5154e87943c45adf1repairman888电脑.png"
                         mode="aspectFill"
@@ -96,7 +96,7 @@
                 </view>
             </view>
             <view style="width:25%;margin-bottom:30rpx;">
-                <view class="avatarWrap" @click="avatar_text2_5_0">
+                <view class="avatarWrap" @click="toApply(6)">
                     <mx-avatar
                         src="https://jfile.cloudpnr.com/app-7349e98b-7bef-4b61-b278-48644bbc8058/b36d3e090f56caca6403b55390198b4repairman888电源，插头.png"
                         mode="aspectFill"
@@ -112,7 +112,7 @@
                 </view>
             </view>
             <view style="width:25%;margin-bottom:30rpx;">
-                <view class="avatarWrap" @click="avatar_text2_6_0">
+                <view class="avatarWrap" @click="toApply(7)">
                     <mx-avatar
                         src="https://jfile.cloudpnr.com/app-7349e98b-7bef-4b61-b278-48644bbc8058/642c4028577534d4d77b9143b2069f79repairman888打印机.png"
                         mode="aspectFill"
@@ -128,7 +128,7 @@
                 </view>
             </view>
             <view style="width:25%;margin-bottom:30rpx;">
-                <view class="avatarWrap" @click="avatar_text2_7_0">
+                <view class="avatarWrap" @click="toApply(0)">
                     <mx-avatar
                         src="https://jfile.cloudpnr.com/app-7349e98b-7bef-4b61-b278-48644bbc8058/ebf1a3361f63249e80c3c4ac47105443repairman888更多.png"
                         mode="aspectFill"
@@ -157,21 +157,20 @@
             paddingBottom="20"
             space="ensp"
         />
-        <view class="imageContentWrap" @click="image_content14_0">
-            <image
-                class="imImg"
-                src="https://jarvisfile-test.oss-cn-shanghai.aliyuncs.com/app-a8187ae7-5671-4888-4321-2cc8c4d476ba/ADAMALL_PIC0089443175550205952.png?x-oss-process=image/resize,m_lfit,w_200"
-            />
+        <view class="imageContentWrap" v-for="item in list">
             <view class="contentWrap">
-                <view class="imTop">
-                    <view class="imTitle">冰箱损坏</view>
-                    <view class="imIntro">普天4楼餐厅</view>
+                <view class="imTitle">{{ item.title }}</view>
+                <view class="tagTxtWrap">
+                    <view>位置：</view>
+                    <view class="imIntro">{{ item.position }}</view>
                 </view>
-                <view class="imBottom">
-                    <view class="tagTxtWrap">
-                        <view class="tagTxt">报修人：李项</view>
-                    </view>
-                    <view class="imPrice">联系电话：18667889099</view>
+                <view class="tagTxtWrap">
+                    <view>报修人：</view>
+                    <view class="tagTxt">{{ item.applyer }}</view>
+                </view>
+                <view class="tagTxtWrap">
+                    <view>联系电话：</view>
+                    <view class="imPrice">{{ item.phone }}</view>
                 </view>
             </view>
         </view>
@@ -186,30 +185,24 @@ export default {
                     'src':
                         'https://jfile.cloudpnr.com/app-7349e98b-7bef-4b61-b278-48644bbc8058/42e1a1131dd0723650893e2252c71180repairman8881c614d78fdeb961216d08594552a9fb4.jpg'
                 }
-            ]
+            ],
+            list: []
         };
     },
     onShow() {
         this.$Monitor && this.$Monitor.pageShow();
+        this.list = [{ title: '桌子损坏了', position: '一楼', applyer: '李名', phone: '18878990989' }];
     },
     onHide() {
         this.$Monitor && this.$Monitor.pageHide();
     },
     props: {},
     methods: {
-        avatar_text2_0_0() {
+        toApply(type) {
             uni.navigateTo({
-                url: '/pages/index/apply'
+                url: '/pages/index/apply?type=' + type
             });
-        },
-        avatar_text2_1_0() {},
-        avatar_text2_2_0() {},
-        avatar_text2_3_0() {},
-        avatar_text2_4_0() {},
-        avatar_text2_5_0() {},
-        avatar_text2_6_0() {},
-        avatar_text2_7_0() {},
-        image_content14_0() {}
+        }
     }
 };
 </script>
@@ -241,6 +234,7 @@ export default {
 .imageContentWrap {
     display: flex;
     padding: 20rpx 20rpx;
+    border-bottom: 6rpx solid #1babf2;
     .imImg {
         width: 280rpx;
         height: 210rpx;
@@ -266,8 +260,8 @@ export default {
             justify-content: space-between;
         }
         .imTitle {
-            font-weight: 400;
-            font-size: 28rpx;
+            font-weight: 700;
+            font-size: 30rpx;
             line-height: 1.2;
             margin-bottom: 10rpx;
         }
@@ -275,13 +269,14 @@ export default {
             color: #7d7d7d;
         }
         .tagTxtWrap {
+            display: flex;
+            margin-bottom: 8rpx;
             .tagTxt {
                 display: inline-block;
                 padding: 6rpx 8rpx;
                 font-size: 28rpx;
                 margin-right: 10rpx;
                 border-radius: 4rpx;
-                background-color: #fde7d9;
                 color: #de6e0d;
             }
         }
